75862d2f59e905daf8b16f6798e1cdd4ff491409
Takeshi <tak@prismify.co>
2025-01-17 08:21:19 -0700

    Add Vitest configuration file to set up test coverage exclusions

---
+++ vitest.config.ts
@@ -0,0 +1,21 @@
+import { defineConfig } from 'vitest/config'
+
+const exclude = [
+  'main.ts',
+  'vite.config.ts',
+  'vitest.config.ts',
+  'node_modules',
+  'docs',
+  'coverage',
+  'dist',
+  'example'
+];
+export default defineConfig({
+  test: {
+    coverage: {
+      provider: 'v8',
+      exclude,
+    },
+    exclude,
+  }
+})
