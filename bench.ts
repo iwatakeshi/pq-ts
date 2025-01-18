82b707fb5277ec1cfddce7edbdddf682131d7824
Takeshi <tak@prismify.co>
2025-01-17 20:00:53 -0700

    Increase benchmark item count to 1,000,000 for more comprehensive performance testing

--- bench.ts
+++ bench.ts
@@ -5,39 +5,39 @@ const pq = new PriorityQueue<number>();
 const spq = new StablePriorityQueue<number>();
 const fpq = new FlatPriorityQueue<number>(Uint32Array, 1000);
 
-const ITEMS_COUNT = 100000;
+const ITEMS_COUNT = 1000000;
 
-bench("PriorityQueue enqueue 100000 items", () => {
+bench(`PriorityQueue enqueue ${ITEMS_COUNT} items`, () => {
   for (let i = 0; i < ITEMS_COUNT; i++) {
     pq.enqueue(Math.random(), Math.random());
   }
 });
 
-bench("PriorityQueue dequeue 100000 items", () => {
+bench(`PriorityQueue dequeue ${ITEMS_COUNT} items`, () => {
   for (let i = 0; i < ITEMS_COUNT; i++) {
     pq.dequeue();
   }
 });
 
-bench("StablePriorityQueue enqueue 100000 items", () => {
+bench(`StablePriorityQueue enqueue ${ITEMS_COUNT} items`, () => {
   for (let i = 0; i < ITEMS_COUNT; i++) {
     spq.enqueue(Math.random(), Math.random());
   }
 });
 
-bench("StablePriorityQueue dequeue 100000 items", () => {
+bench(`StablePriorityQueue dequeue ${ITEMS_COUNT} items`, () => {
   for (let i = 0; i < ITEMS_COUNT; i++) {
     spq.dequeue();
   }
 });
 
-bench("FlatPriorityQueue enqueue 100000 items", () => {
+bench(`FlatPriorityQueue enqueue ${ITEMS_COUNT} items`, () => {
   for (let i = 0; i < ITEMS_COUNT; i++) {
     fpq.enqueue(Math.random(), Math.random());
   }
 });
 
-bench("FlatPriorityQueue dequeue 100000 items", () => {
+bench(`FlatPriorityQueue dequeue ${ITEMS_COUNT} items`, () => {
   for (let i = 0; i < ITEMS_COUNT; i++) {
     fpq.dequeue();
   }
