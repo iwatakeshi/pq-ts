6f9700dc98fa7cb833c5a4244ae9d0c658cffe57
iwatakeshi <iwatakeshi@gmail.com>
2025-01-18 22:12:30 -0700

    Update exports in main.ts to include typed priority queue implementations

--- main.ts
+++ main.ts
@@ -2,5 +2,5 @@ export * from "./src/types.ts";
 export * from "./src/primitive.ts";
 export * from "./src/pq.ts";
 export * from "./src/stable.pq.ts";
-export * from "./src/flat.pq.ts";
-export * from "./src/stable.flat.pq.ts";
+export * from "./src/typed.pq.ts";
+export * from "./src/stable.typed.pq.ts";
